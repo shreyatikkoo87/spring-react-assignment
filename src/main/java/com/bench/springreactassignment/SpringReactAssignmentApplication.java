4f929c6edbe1d980189bc43b2806ad59b1a97353
CRB\STI111 <121609911+shreyatikkoo87@users.noreply.github.com>
2024-01-19 19:19:16 +0530

    assignment

---
+++ src/main/java/com/bench/springreactassignment/SpringReactAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.bench.springreactassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bench/springreactassignment/SpringReactAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bench.springreactassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
