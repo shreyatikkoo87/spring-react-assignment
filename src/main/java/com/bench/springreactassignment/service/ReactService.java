6d4d8da83bf776a3a2b0d50e5233c800572a4cc2
CRB\STI111 <121609911+shreyatikkoo87@users.noreply.github.com>
2024-01-20 15:40:31 +0530

    react controller and service

--- src/main/java/com/bench/springreactassignment/service/ReactService.java
+++ src/main/java/com/bench/springreactassignment/service/ReactService.java
@@ -2,6 +2,7 @@ package com.bench.springreactassignment.service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -27,7 +28,57 @@ public class ReactService {
 
     public Flux<String> namesMono_map_filter_switch_if_empty(int i) {
         Flux<String> s = Flux.just("Shreya","Mayank","Aarya");
-        return s.map(String::toUpperCase).filter(e -> e.length()>i).switchIfEmpty(Mono.just("no data found"));
+        return s.map(String::toUpperCase).filter(e -> e.length()>i).switchIfEmpty(Flux.just("no data found"));
+    }
+/*
+    public Flux<String> namesFlux_flatMapMany() {
+        Flux<String> s = Flux.just("Shreya","Mayank","Aarya");
+         s.flatM;
+    }*/
+
+    public Mono<String> demoFlatMap(){
+        Mono<String> name = Mono.just("shreya");
+        return name.flatMap(ReactService::transformUpperCase);
+    }
+
+    public static Mono<String> transformUpperCase(String s){
+       String name = s.toUpperCase();
+       return Mono.just(name);
+    }
+
+    public Flux<String> demoFlatMapFlux(){
+        Flux<String> name = Flux.just("shreya","mayank");
+        return name.flatMap(ReactService::transformUpperCaseFlux);
+    }
+
+    private static Flux<String> transformUpperCaseFlux(String s) {
+        String name = s.toUpperCase();
+        return Flux.just(name);
+    }
+
+    public Flux<String> demoConcatStreamsStatic(){
+        Flux<String> name1 = Flux.just("shreya","mayank");
+        Flux<String> name2 = Flux.just("usha","ashok");
+        name1.concatWith(name2);
+        return Flux.concat(name1,name2);
+    }
+
+    public Flux<String> demoConcatStreamsNonStatic(){
+        Flux<String> name1 = Flux.just("shreya","mayank");
+        Flux<String> name2 = Flux.just("usha","ashok");
+        return name1.concatWith(name2);
+    }
+
+    public Flux<String> demoMergeStreamsNonStatic(){
+        Flux<String> name1 = Flux.just("shreya","mayank");
+        Flux<String> name2 = Flux.just("usha","ashok");
+        return name1.mergeWith(name2);
+    }
+
+    public Flux<String> demoMergeStreamsStatic(){
+        Flux<String> name1 = Flux.just("shreya","mayank");
+        Flux<String> name2 = Flux.just("usha","ashok");
+        return Flux.merge(name1,name2);
     }
 
 
---
+++ src/test/java/com/bench/springreactassignment/service/ReactServiceTest.java
@@ -0,0 +1,72 @@
+package com.bench.springreactassignment.service;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReactServiceTest {
+    ReactService service = new ReactService();
+
+
+
+
+    @Test
+    void namesMono_map() {
+        ReactService reactService = new ReactService();
+        Mono<String> response = reactService.namesMono_map(10);
+        StepVerifier.create(response).expectNext("TEST").verifyComplete();
+    }
+
+    @Test
+    void demoFlatMap() {
+        ReactService reactService = new ReactService();
+        Mono<String>name = reactService.demoFlatMap();
+        StepVerifier.create(name).expectNext("SHREYA").verifyComplete();
+    }
+
+    @Test
+    void demoFlatMapFlux() {
+        ReactService reactService = new ReactService();
+        Flux<String> names = reactService.demoFlatMapFlux();
+        StepVerifier.create(names).expectNext("SHREYA","MAYANK").verifyComplete();
+
+
+    }
+
+    @Test
+    void namesMono_map_filter_switch_if_empty() {
+        Flux<String> names = service.namesMono_map_filter_switch_if_empty(10);
+        StepVerifier.create(names).expectNext("no data found").verifyComplete();
+    }
+
+
+    @Test
+    void demoConcatStreams() {
+        Flux<String> names = service.demoConcatStreamsStatic();
+        StepVerifier.create(names).expectNext("shreya","mayank","usha","ashok").verifyComplete();
+    }
+    @Test
+    void demoConcatStreamsNonStatic() {
+        Flux<String> names = service.demoConcatStreamsNonStatic();
+        StepVerifier.create(names).expectNext("shreya","mayank","usha","ashok").verifyComplete();
+    }
+
+    @Test
+    void demoMergeStreamsNonStatic() {
+        Flux<String> names = service.demoMergeStreamsNonStatic();
+        StepVerifier.create(names).expectNext("shreya","mayank","usha","ashok").verifyComplete();
+    }
+
+
+    @Test
+    void demoMergeStreamsStatic() {
+        Flux<String> names = service.demoMergeStreamsStatic();
+        StepVerifier.create(names).expectNext("shreya","mayank","usha","ashok").verifyComplete();
+    }
+}
